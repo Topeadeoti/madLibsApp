0e3cf51d0c04845ddf72baf03e10c4d4a0521321
Topeadeoti <taibattemitope@gmail.com>
2023-03-15 20:04:08 +0100

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+//to store userinputs
 let userInputs = [];
+
+//originalstory
 let originalStory = `Once upon a time, there was a ${userInputs[0]} named ${userInputs[1]} who ${userInputs[2]} in a ${userInputs[3]}${userInputs[4]} that was ${userInputs[5]} off from the rest of the ${userInputs[6]}.${userInputs[7]} had always been ${userInputs[8]} by technology, but living in such a remote location meant that he never had the chance to ${userInputs[9]} it firsthand.`
 
-console.log(originalStory);
+//output originalstory
+console.log(originalStory);  
+
+//record the number of unique replacement
+let numberOfQuestions = "10";
+
+
 
 
 
